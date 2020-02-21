588fbe5895b00ddb048f189097cd582fbb756ad6
John Koht <john@kohactive.com>
2020-02-20 21:37:17 -0600

    clean up

--- variants/bootstrap/app/assets/stylesheets/application.scss.rb
+++ variants/bootstrap/app/assets/stylesheets/application.scss.rb
@@ -1,7 +1,6 @@
 prepend_to_file "app/assets/stylesheets/application.scss" do
   <<-SCSS.strip_heredoc
     //= require ./bootstrap
-    //= require rails_bootstrap_forms
     //= require font-awesome
   SCSS
 end
